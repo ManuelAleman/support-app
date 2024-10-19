c5848d6318b46a23bc2bef82795aaa83b6c4815f
ManuelAleman <manuelitoaleman230@icloud.com>
2024-10-18 23:35:17 -0700

    LOADING APP SOLVE

--- support-app/src/pages/_app.tsx
+++ support-app/src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { UserProvider } from './../utils/UserContext';
 import Footer from '@/components/Footer';
+import { useUser } from './../utils/UserContext';
+import LoadingPage from './LoadingPage';
+
 function MyApp({ Component, pageProps } : any) {
+    const { user, setUser, loading } = useUser();
+
     return (
         <UserProvider>
             <Component {...pageProps} />
-            <Footer />
+            {!loading && <Footer />}
         </UserProvider>
     );
 }
